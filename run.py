f51c68a84e1bbe56eecd3c3479d371d968fdb154
Derek Schuster <dschust1@binghamton.edu>
2016-01-16 21:46:38 -0500

    added structure

--- run.py
+++ run.py
@@ -2,9 +2,17 @@ from flask import *
 
 app = Flask(__name__)
 
+def get_data():
+	pass
+
+def parse_data(data):
+	pass
+
 @app.route('/')
 def index():
-	return render_template('index.html')
+	data = get_data()
+	_bars = parse_data(data)
+	return render_template('index.html', bars=_bars)
 
 if __name__ == '__main__':
 	#app.run(host='0.0.0.0')
