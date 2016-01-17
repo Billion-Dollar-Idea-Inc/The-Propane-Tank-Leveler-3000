1b751b6193e44aa5f911a868746b29251802c25b
Derek Schuster <dschust1@binghamton.edu>
2016-01-17 10:17:31 -0500

    website and format

--- run.py
+++ run.py
@@ -25,64 +25,64 @@ def get_data():
 	pass
 
 def parse_data(raw_data):
-        sensors = {}
+    sensors = {}
 
-        data = raw_data["samples"]
-        for point in data:
-            if not point["channel"] in sensors:
-                sensors[point["channel"]] = [point["d"]]
-            else:
-                sensors[point["channel"]].append(point["d"])
+    data = raw_data["samples"]
+    for point in data:
+        if not point["channel"] in sensors:
+            sensors[point["channel"]] = [point["d"]]
+        else:
+            sensors[point["channel"]].append(point["d"])
 
-        for sensor in sensors:
-            numPoints = len(sensor)
-            total = 0
-            for val in sensor:
-                total = total + val
-            average = total / numPoints
-            sensors[sensor] = average
+    for sensor in sensors:
+        numPoints = len(sensor)
+        total = 0
+        for val in sensor:
+            total = total + val
+        average = total / numPoints
+        sensors[sensor] = average
 
-        tolerance = .05
-        analyzed_data = {}
-        for sensor in sensors:
-            analyzed_data[sensor] = false #default to false, if sensor is at level with propane value will be set to true
+    tolerance = .05
+    analyzed_data = {}
+    for sensor in sensors:
+        analyzed_data[sensor] = false #default to false, if sensor is at level with propane value will be set to true
 
-        keys = sensors.keys()
-        for i in range(len(keys)):
-            if not i == 0:
-                if sensors[sensor] - sensors[sensor] * tolerance < sensors[keys[i-1]] and sensors[keys[i-1]] < sensors[sensor] + sensors[sensor] * tolerance:
-                    analyzed_data[keys[i]] = true
-                else:
-                    break;
-            else:
+    keys = sensors.keys()
+    for i in range(len(keys)):
+        if not i == 0:
+            if sensors[sensor] - sensors[sensor] * tolerance < sensors[keys[i-1]] and sensors[keys[i-1]] < sensors[sensor] + sensors[sensor] * tolerance:
                 analyzed_data[keys[i]] = true
+            else:
+                break;
+        else:
+            analyzed_data[keys[i]] = true
 
-        num_true = 0
-        for i in analyzed_data:
-            if analyzed_data[i]:
-                num_true = num_true + 1
+    num_true = 0
+    for i in analyzed_data:
+        if analyzed_data[i]:
+            num_true = num_true + 1
 
-        fill = num_true/len(sensors)
-        fill = fill*4
-        fill = int(fill)
+    fill = num_true/len(sensors)
+    fill = fill*4
+    fill = int(fill)
 
-	return fill
+    return fill
 
 @app.route('/')
 @app.route('/index/')
 def index():
 	#data = get_data()
 	#_bars = parse_data(data)
-        
-        dic_pic = {
-            '0': 'static/images/propaneEmpty.png',
-            '1': 'static/images/propaneRed.png',
-            '2': 'static/images/propaneOrange.png',
-            '3': 'static/images/propaneYellow.png',
-            '4': 'static/images/propaneGreen.png'
-        }
 
-	return render_template('index.html', image = dic_pic[str(parse_data(data))])
+    dic_pic = {
+        '0': 'static/images/propaneEmpty.png',
+        '1': 'static/images/propaneRed.png',
+        '2': 'static/images/propaneOrange.png',
+        '3': 'static/images/propaneYellow.png',
+        '4': 'static/images/propaneGreen.png'
+    }
+
+    return render_template('index.html', image = dic_pic[str(1)])
 
 @app.errorhandler(404)
 def page_not_found(error):
